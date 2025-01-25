dfb8cb260ac93ce3443dba2381f82b65c5607f35
singhAvira <135752497+singhAvira@users.noreply.github.com>
2025-01-25 10:50:40 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score ="33"
+// let score ="33abs";
+// let score =null//it gives output =0;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valuInNumber =Number(score)//it convert it into number 
+// console.log(typeof valuInNumber);//Nan(not a number)
+
+//"33"=>33
+//"33abc =>NaN
+//true=>1; false=>0;
+// let isLoggedIn=1
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//1=> true; 0=>false
+//""=> false (boolean conversion gives output false)
+//"hitesh"=>true;
+let someNumber =33
+let stringNumber=String(someNumber)//covert into string
+console.log(stringNumber);
+console.log(typeof stringNumber);
+//browser always give you string value so you needed to convert it into no. booleans,object,array etc..
