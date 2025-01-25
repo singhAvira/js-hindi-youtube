7ae9e9dc7d5388ef0d4e6076add30f877dbdac90
singhAvira <135752497+singhAvira@users.noreply.github.com>
2025-01-25 09:50:44 +0000

    datatypes

---
+++ 01_basics/02_dataTyes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3+3)
+console.log("Hitesh")
+
+let name="Aviral"
+let age=18
+let isLoggedIn= false
+
+//number=>2 to powet 53 
+//bigint for bog intergers
+//string =>""
+//boolean=> true/false
+//null (its a standalone value hai ) null ak reprsentation hai empty value ka(let state =null)(null means empty)(null is a object)
+//undefined =>
+//symbol 
+
+//object
+console.log(typeof "aviral")//gives string as the anwers
+console.log(typeof age);//number
+console.log(typeof undefined)//undefined
+console.log(typeof null)//ouput is object
