b8b7ec063e239dacd51d2c5c7190ce4348deba44
singhAvira <135752497+singhAvira@users.noreply.github.com>
2025-01-27 20:43:38 +0000

    Arrays 01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+//array
+
+const myArr =[0,1,2,3,4,5]
+const myheros =["shaktiman","naagraj"]
+
+const myArr2= new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//Array method
+// myArr.push(6)
+// myArr.push(7);
+// myArr.pop()
+
+// myArr.unshift(9)
+//myArr.shift()
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+ 
+const newArr= myArr.join()//join convert the array into string array;
+
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);//string as ans
+
+//slice ,splice
+
